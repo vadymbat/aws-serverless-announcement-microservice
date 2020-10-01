9c1d89593da39278eb6101a4488d336057a81ea2
Vadym Batov <vadymbat@gmail.com>
2020-10-01 13:00:59 +0300

    update deploy/cleanup scripts

---
+++ cleanup.sh
@@ -0,0 +1,24 @@
+# the script expect to get aws-cli configs via Environment variables
+# export AWS_DEFAULT_REGION='eu-central-1'
+# export AWS_ACCESS_KEY_ID=''
+# export AWS_SECRET_ACCESS_KEY=''
+# export AWS_SESSION_TOKEN='''
+
+CONFIG_S3_BUCKET='aws-serverless-announcement-microservice'
+CF_STACK_NAME='announcment-app'
+log_group_name_patern='announcement-app'
+
+
+aws cloudformation delete-stack \
+    --stack-name "${CF_STACK_NAME}" &> /dev/null
+echo "deleted stack ${CF_STACK_NAME}"
+
+log_groups_to_delete=$(aws logs describe-log-groups \
+    --query "logGroups[? contains(logGroupName, '${log_group_name_patern}')].logGroupName" \
+    --output text)
+for log_group in ${log_groups_to_delete}; do
+    aws logs delete-log-group --log-group-name "${log_group}" &> /dev/null
+    echo "deleted log group ${log_group}"
+done
+
+aws s3 rb s3://${CONFIG_S3_BUCKET} --force
--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,25 @@
-CONFIG_S3_BUCKET=''
-export AWS_DEFAULT_REGION=''
+# the script expect to get aws-cli configs via Environment variables
+# export AWS_DEFAULT_REGION='eu-central-1'
+# export AWS_ACCESS_KEY_ID=''
+# export AWS_SECRET_ACCESS_KEY=''
+# export AWS_SESSION_TOKEN='''
+
+CONFIG_S3_BUCKET='aws-serverless-announcement-microservice'
+CF_STACK_NAME='announcment-app'
+
+aws s3 mb s3://${CONFIG_S3_BUCKET}
 
 aws cloudformation package \
-    --template-file microservice.yaml \
+    --template-file announcement_app.yaml \
     --s3-bucket $CONFIG_S3_BUCKET \
     --output-template-file processed_template.yaml
 
 aws cloudformation deploy \
     --template-file processed_template.yaml \
-    --stack-name anouncment-app \
+    --stack-name ${CF_STACK_NAME} \
     --capabilities CAPABILITY_IAM
+
+api_endpoint=$(aws cloudformation describe-stacks --stack-name "${CF_STACK_NAME}" \
+    --query "Stacks[].Outputs[? OutputKey == 'ApiUrl'].OutputValue" \
+    --output text)
+echo "The API is available at ${api_endpoint}"
