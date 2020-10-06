6ee3e0261741b5146b6ba500d55bbff5d5a1c2d0
Vadym Batov <vadymbat@gmail.com>
2020-10-06 16:09:42 +0300

    format the code

--- api/handler.py
+++ api/handler.py
@@ -4,35 +4,27 @@ import helper
 
 
 def create_announcement(event, context):
-    status_code = 500
     try:
         item = helper.generate_announcement(event['body'])
         helper.dynamodb_put_item(item)
-        status_code = 201
-        response = helper.generate_response(status_code, item)
+        response = helper.generate_response(status_code=201, body=item)
     except ValueError as e:
-        error_message = "Received malformed announcement date."
-        logging.error(error_message)
+        error_message = "Received malformed announcement."
         logging.error(e)
-        response = helper.generate_error(status_code, error_message)
+        response = helper.generate_error(status_code=400, message=error_message)
     except Exception as e:
-        error_message = "Some error happend, please check the" + \
-            " announcement syntax or try again later."
-        logging.error(error_message)
+        error_message = "Unknown error happened, please contact the support team."
         logging.error(e)
-        response = helper.generate_error(status_code, error_message)
+        response = helper.generate_error(status_code=500, message=error_message)
     return response
 
 
 def list_announcements(event, context):
     try:
         items = helper.dynamodb_list_items()
-        status_code = 200
-        response = helper.generate_response(status_code, items)
+        response = helper.generate_response(status_code=200, body=items)
     except Exception as e:
-        status_code = 500
-        error_message = 'Cannot list announcements, some error happend.'
-        logging.error(error_message)
+        error_message = 'Cannot list announcements, please contact the support team.'
         logging.error(e)
-        response = helper.generate_error(status_code, error_message)
+        response = helper.generate_error(status_code=500, message=error_message)
     return response
